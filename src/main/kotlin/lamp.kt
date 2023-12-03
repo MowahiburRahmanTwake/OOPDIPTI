7e103f0ac6658e73ae006cc5426b3c0567d02987
twake1234 <twake.edu@gmail.com>
2023-12-03 13:08:48 +0600

    Class 16

---
+++ src/main/kotlin/lamp.kt
@@ -0,0 +1,13 @@
+class Person{
+    var firstName1:String = ""
+    var age1:Int = 0
+    constructor(firstName: String, age:Int){
+        this.firstName1 = firstName
+        this.age1 = age
+    }
+    constructor()
+    constructor(firstName: String){
+        this.firstName1 = firstName
+
+    }
+}
